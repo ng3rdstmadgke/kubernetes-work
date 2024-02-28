788944c14235b6ef0c0b2d988e90155db3d8f87e
ng3rdstmadgke <ng3rdstmadgke@gmail.com>
2024-02-28 14:46:41 +0900

    terraform-eksバックアップ

---
+++ chapter_03/terraform-eks.back/provider.tf
@@ -0,0 +1,57 @@
+terraform {
+  required_version = "~> 1.7"
+  backend "s3" {
+
+    # TODO 自分のS3バケット名に置き換えます。
+    bucket = "kubernetes-work-tfstate"
+    key    = "terraform.tfstate"
+    region = "ap-northeast-1"
+  }
+
+  required_providers {
+    aws = {
+      // https://registry.terraform.io/providers/hashicorp/aws/latest
+      source  = "hashicorp/aws"
+      version = "~> 5.38.0"
+    }
+    kubernetes = {
+      // https://registry.terraform.io/providers/hashicorp/kubernetes/latest
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.26.0"
+    }
+    helm = {
+      // https://registry.terraform.io/providers/hashicorp/helm/latest
+      source  = "hashicorp/helm"
+      version = "~> 2.12.0"
+    }
+  }
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.eks.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.eks.token
+    config_path            = "~/.kube/config"
+  }
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.eks.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.eks.token
+}
+
+provider "aws" {}
+
+output "cluster_id" {
+  value = module.eks.cluster_id
+}
+
+output "cluster_name" {
+  value = module.eks.cluster_name
+}
+
+output "cluster_endpoint" {
+  value = data.aws_eks_cluster.eks.endpoint
+}
